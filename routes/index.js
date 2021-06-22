8c24234e4feebb4c9d02b8d88097199b079ec948
Aaqib-Ahmad <82023206+Aaqib-Ahmad@users.noreply.github.com>
2021-06-22 16:31:26 +0500

    C

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var Login=require("../models/login")
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wellcome to Shopping Site' });
-});
-module.exports = router;
